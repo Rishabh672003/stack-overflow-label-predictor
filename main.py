6af37d184a13a7d52fae44fd14079c4b8c7dbe4c
Rishabh <53911515+Rishabh672003@users.noreply.github.com>
2024-07-30 20:29:45 +0530

    now this finally works, lets see if the deployment works

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-import streamlit as st
-from pydantic import BaseModel
-import numpy as np
-import tensorflow as tf
 import re
 import string
+
+import streamlit as st
+import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
+
+import numpy as np
 
 
 # Define the custom standardization function
@@ -16,39 +17,53 @@ def custom_standardization(input_data):
         stripped_html, "[%s]" % re.escape(string.punctuation), ""
     )
 
+
 vectorize_layer = layers.TextVectorization(
     standardize=custom_standardization,
     max_tokens=3000,
-    output_mode='int',
-    output_sequence_length=300)
-
-# Load the model with the custom standardization function
-loaded_model = keras.models.load_model('./notebooks/NLP-model.keras')
+    output_mode="int",
+    output_sequence_length=300,
+)
 
 
-class TextInput(BaseModel):
-    text: str
-
-
-@st.cache_data
-def predictor(input):
-    labels = ["Csharp", "Java", "Javascript", "Python"]
-    examples = input
-    examples = vectorize_layer(examples)
-    answers = loaded_model.predict(examples)
-    label = np.argmax(answers)
-    return labels[label]
-
-
-st.title("Text Predictor")
-text_input = st.text_area("Enter your question here:", height=150)
-if st.button("Submit"):
-    if text_input == "":
-        st.error(
-            "Please enter a question!"
-        )  # Display error message for empty input
-    else:
-        processed_text = predictor(text_input)
-        st.write(
-            f"Model predicted the question is for the following language: {processed_text}"
+# Load the TensorFlow model (assuming the model is saved at 'NLP-keras.keras')
+@st.cache_resource
+def load_model():
+    try:
+        return keras.models.load_model(
+            "./models/NLP-model.keras",
+            custom_objects={"custom_standardization": custom_standardization},
         )
+
+    except Exception as e:
+        st.error(f"Error loading model: {e}")
+        return None
+
+
+loaded_model = load_model()
+
+
+# Ensure the model is loaded before proceeding
+if loaded_model is not None:
+    # Define the predictor function
+    @st.cache_data
+    def predictor(input_text):
+        labels = ["Csharp", "Java", "Javascript", "Python"]
+        input = tf.constant([input_text])
+        answers = loaded_model.predict(input)
+        label = np.argmax(answers)
+        return labels[label]
+
+    # Streamlit app interface
+    st.title("Text Predictor")
+    text_input = st.text_area("Enter your question here:", height=150)
+    if st.button("Submit"):
+        if text_input.strip() == "":
+            st.error("Please enter a question!")
+        else:
+            processed_text = predictor(text_input)
+            st.write(
+                f"Model predicted the question is for the following language: {processed_text}"
+            )
+else:
+    st.error("Model could not be loaded.")
